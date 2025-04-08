6d819fa689e78217fa79ea857f839b6f43e80a20
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-08 08:16:59 +0000

    adding two numbers

---
+++ 4-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+# iwant  devolp a program that can add 2 numbers
+NUMBER1=$1
+NUMBER2=$2
+#i need run the command to add 2 numbers
+SUM=$((NUMBER1+NUNUMBER2))
+echo "addition of 2 numbers:$SUM"
+
