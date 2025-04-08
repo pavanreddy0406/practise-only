21730e01207998a2b015878e98705ff648245dcb
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-08 07:52:05 +0000

    instead of value replace with variable name pavan and sai

--- 1-hello-world.sh
+++ 1-hello-world.sh
@@ -1,2 +1,11 @@
 #!/bin/bash/
 echo "pavan learning devops with aws"
+PERSON1=pavan
+PERSON2=sai
+
+echo "PERSON1:how are you PERSON2"
+echo "PERSON2:iam fine PERSON1"
+echo "PERSON1:yekkada unnav PERSON2"
+echo "PERSON2: nenu hostel lo unna PERSON1"
+echo "PERSON1:ok PERSON2"
+echo "PERSON2:ok PERSON1"
