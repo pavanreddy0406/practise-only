2e23e7aa0b68f971f51fa24da221fa231a1b0217
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-08 08:27:51 +0000

    entering username and password

---
+++ 5-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+
+echo "please enterd your username"
+read -e  USERNAME
+echo "username is enterd :$USERNAME"
+echo "please entered your password"
+read -e  PASSWORD
+echo "password is enterd is:$PASSWORD"
