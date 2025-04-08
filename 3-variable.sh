d21e0167eecbc1bbb2720b93f20de47eb7ab02ed
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-08 08:09:02 +0000

    get values of variables from command line

---
+++ 3-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+echo "pavan learning devops with aws"
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:how are you $PERSON2"
+echo "$PERSON2:iam fine $PERSON1"
+echo "$PERSON1:yekkada unnav $PERSON2"
+echo "$PERSON2: nenu hostel lo unna $PERSON1"
+echo "$PERSON1:ok $PERSON2"
+echo "$PERSON2:ok $PERSON1"
