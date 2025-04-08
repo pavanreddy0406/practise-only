e70f34c507c0fba0f05f581d30ade73892188dc5
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-08 10:33:31 +0000

    number is greater than 10 or n0t

--- 7-variable.sh
+++ 7-variable.sh
@@ -1,9 +1,10 @@
 #!/bin/bash/
 #check the number is greater than 10
 NUMBER=$1
+
 if [ $NUMBER -gt 0 ]
 then 
     echo "$NUMBER is gretarer than 10"
 else 
     echo "$NUMBER is not greater than 10"
-    
+fi
