0db382710815b8428e33cb84ac9aa483c739fed6
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-09 06:23:28 +0000

    checking root

--- 11-loops.sh
+++ 11-loops.sh
@@ -3,10 +3,9 @@ USERID=(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-
 if [ $USERID -ne 0 ];
 then
-    echo "$R ERROR:please run with root acess $N"
+    echo -e "$R ERROR:please run with root acess $N"
     exit 1
 fi
 
