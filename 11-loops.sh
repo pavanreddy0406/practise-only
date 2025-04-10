cd5f503eecc7127c92a2d66a225592709255326e
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-10 03:34:27 +0000

    logfiles

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,45 +1,44 @@
-#!/bin/bash/
-DATE=$(date +%F)
-LOGDIR=/home/centos/shellscript-logs
-SCRIPT_NAME=$0
-LOGFILE=$LOGDIR/$0-$DATE.log
+#!/bin/bash
 
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+N="\e[0m"
 Y="\e[33m"
-N=\e[0m"
-VALIDATE (){
-if [ $i -ne 0 ]
-then
-    echo -e " installing $2...$R failure $N"
-    exit 1
 
-else
-    echo -e " installing $2 ... $G sucess $N"
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
 fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
 }
 
-
-
-if[ $USERID -ne 0 ];
-then
-    echo "$R ERROR:please go with root acess $N"
-    exit 1
-fi
-# all args in $@
-
+# all args are in $@
 for i in $@
 do
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
 
-yum installed $i &>>LOGFILE
- if [ $i -ne 0 ]
- then
-     echo "$i is not installed : lets install"
-     exit 1
-     yum install $i -y &>>LOGFILE
-     VALIDATE $? "$i"
-else
-   echo -e "$Y $i is alredy installed $N"
-fi
+    #yum install $i -y
 done
