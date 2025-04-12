256bd05d19ceced756e36272637bf9e2c61cff19
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-12 11:11:00 +0000

    mail

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash/
-DATE=$(DATE +%F:+%H:+%M:+%S)
+DATE=$(date +%F:+%H:+%M:+%S)
 LOGFILE_DIR=/tmp
 SCRIPT_NAME=$0
 LOGFILE=$LOGFILE_DIRCTORY/$SCRIPT_NAME-$DATE.log
@@ -10,6 +10,7 @@ N="\e[0m"
 Y="\e[33m"
 DISK_USAGE=$(df -hT |grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
+message=
  
 while IFS= read line
 do
@@ -17,8 +18,8 @@ usage=$(echo $line |awk '{print $6}'|cut -d % -f1)
 partition=$(echo $line |awk '{print $1}')
 if { $usage -gt $DISK_USAGE_THRESHOLD };
 then
-message+="high disk usage on $partition:$usage"
+    message+="high disk usage on $partition:$usage"
 
 done <<< $DISK_USAGE
 
-echo "MESSAGE: $message"
+echo "message: $message"
