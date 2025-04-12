9c34e1d35b5979a5bf34bc6ec4efec5dbd8ba4db
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-12 10:53:29 +0000

    mail

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,25 +1,18 @@
 #!/bin/bash/
-DATE=$(date +%F:+%H:+%M:+%S)
-LOGFILE_DIRECTORY=/tmp
-# /home/centos/shellscript-logs/script-name-date.log
+DATE=$(DATE +%F:+%H:+%M:+%S)
+LOGFILE_DIR=/tmp
 SCRIPT_NAME=$0
-LOGFILE=$LOGFILE_DIR/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGFILE_DIRCTORY/$SCRIPT_NAME-$DATE.log
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-Y="\e[33m" 
-DISK_USAGE=$(df -hT|grep -vE 'tmpfs|Filesystem')
+Y="\e[33m"
+DISK_USAGE=$(df -hT |grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
+ 
 while IFS= read line
 do
-usage=$(echo $line |awk '{print$6}' |cut -d  % -f1)
-partition=$(echo $line |awk '{print $1}')
-if [ $usage -gt $DISK_USAGE_THRESHOLD ];
-then
-     message+="HIGH DISK USAGE $partition: $usage"
-fi
+echo "output: $line
 done <<< $DISK_USAGE
-echo -e "message: $message"
 
-#echo "$message" | mail -s "high disk usage" pavanreddyyeddala@gmail.com
-sh mail.sh pavanreddyyeddala@gmail.com "high disk usage" "$message" "devops team" "high disk usage"
