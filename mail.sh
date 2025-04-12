4ae51fac61665d6802f84879f0b61664a76666a7
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-12 06:40:13 +0000

    mail

--- mail.sh
+++ mail.sh
@@ -1,11 +1,12 @@
 #!/bin/bash/
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e s[]\/$*.^[]/\\&/g' <<< $3)
+echo "escaped content: $BODY"
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
 #echo "all args: $@"
 #echo "$BODY | mail -s "$SUBJECT" $TO_ADDRESS
-FINAL_BODY=$(sed -e 's/TEAM_NAME/devops team/g' -e 's/ALRET_TYPE/high disk usage/g' -e "s/message/$BODY/g" template.html)
+FINAL_BODY=$(sed -e 's/TEAM_NAME/$TEAM_NAME/g' -e 's/ALRET_TYPE/$ALRET_TYPE/g' -e "s/MESSAGE/$BODY/g" template.html)
 echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
