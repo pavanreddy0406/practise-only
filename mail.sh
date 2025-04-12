0f5eaaa6ed0be87a6262977a26c55b9bd02b8ffe
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-12 06:26:41 +0000

    mail

--- mail.sh
+++ mail.sh
@@ -5,4 +5,7 @@ BODY=$3
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-echo "all args: $@"
+#echo "all args: $@"
+#echo "$BODY | mail -s "$SUBJECT" $TO_ADDRESS
+FINAL_BODY=$(sed -e 's/TEAM_NAME/devops team/g' -e 's/ALRET_TYPE/high disk usage/g' -e "s/message/$BODY/g" template.html)
+echo "$FINAL_BODY | mail -s "$SUBJECT" $TO_ADDRESS
